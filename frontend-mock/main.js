916080a8111bc6062f37ee73af56b91ba2bd60e5
chalexIAC <lbw@yandex.ru>
2016-10-27 20:43:45 +0300

    fst

---
+++ frontend-mock/main.js
@@ -0,0 +1,8 @@
+$(function () {
+	$('.dropdown-button').dropdown({
+		belowOrigin: false, // Displays dropdown below the button
+		constrain_width: false
+	});
+	$('select').material_select();
+
+});
